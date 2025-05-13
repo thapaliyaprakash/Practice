404a7e28c59a32510ba3ca4f4c47da9ec3286610
Prakash Thapaliya <111227079+thapaliyaprakash@users.noreply.github.com>
2025-05-13 00:22:45 -0900

    mycodespaces added

---
+++ codespaces-react/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          Prakash Thapaliya <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Prakash Thapaliya is a student of BIM at ASMT from TU, Nepal.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://youtube.com/shorts/H-KoHDEDLgU?si=gkpgS9kGHyso0EfQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Subscribe to my YouTube channel
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
